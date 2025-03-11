14d2c06c742d2a5d4d895378e698d6289f8637a9
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-11 11:06:42 +0800

    added deployment dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/sentim-web'
 })
